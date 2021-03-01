64acb87e7cb9af4769b40ccba41de14e8d5bc208
Jack Hilscher <jhilscheriv@gmail.com>
2021-02-28 23:41:06 -0500

    altered array comparison tests from toBe to toEqual

--- loops-2/challenge.test.js
+++ loops-2/challenge.test.js
@@ -1,33 +1,33 @@
 const zeroChecker = require('./challenge')
 
 test('zeroChecker([0]) returns [0]', () => {
-	expect(zeroChecker([0])).toBe([0])
+	expect(zeroChecker([0])).toEqual([0])
 })
 
 test('zeroChecker([1]) returns []', () => {
-	expect(zeroChecker([1])).toBe([])
+	expect(zeroChecker([1])).toEqual([])
 })
 
 test('zeroChecker([1, 1, 0]) returns [2]', () => {
-	expect(zeroChecker([1, 1, 0])).toBe([2])
+	expect(zeroChecker([1, 1, 0])).toEqual([2])
 })
 
 test('zeroChecker([1, 1, 0, 1, 1]) returns [2]', () => {
-	expect(zeroChecker([1, 1, 0, 1, 1])).toBe([2])
+	expect(zeroChecker([1, 1, 0, 1, 1])).toEqual([2])
 })
 
 test('zeroChecker([0, 1, 0, 1, 0]) returns [0, 2, 4]', () => {
-	expect(zeroChecker([0, 1, 0, 1, 0])).toBe([0, 2, 4])
+	expect(zeroChecker([0, 1, 0, 1, 0])).toEqual([0, 2, 4])
 })
 
 test('zeroChecker([0, 1, 1, 1, 1]) returns [0]', () => {
-	expect(zeroChecker([0, 1, 1, 1, 1])).toBe([0])
+	expect(zeroChecker([0, 1, 1, 1, 1])).toEqual([0])
 })
 
 test('zeroChecker([1, 1, 1, 1, 1, 1]) returns []', () => {
-	expect(zeroChecker([1, 1, 1, 1, 1, 1])).toBe([])
+	expect(zeroChecker([1, 1, 1, 1, 1, 1])).toEqual([])
 })
 
 test('zeroChecker([0, 0, 0, 0]) returns [0, 1, 2, 3]', () => {
-	expect(zeroChecker([0, 0, 0, 0])).toBe([0, 1, 2, 3])
+	expect(zeroChecker([0, 0, 0, 0])).toEqual([0, 1, 2, 3])
 })
